6e1f55f1e765ac9066a6cb65712a3e49d0f43c36
Gabriel Patrício <gabrieltp087@gmail.com>
2023-06-15 23:08:46 -0300

    Prepare project to build

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+        filename: env.DATABASE_URL,
+      }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     directory: './db/migrations',
